2b953d3790f301281156d525c63e2cddd6b800c2
Mohamed DCHANE <mdchane@e3r3p8.42.fr>
2018-12-26 12:21:19 +0100

    o et u a faire

--- main.c
+++ main.c
@@ -18,100 +18,100 @@ int		main()
 {
 	setlocale(LC_ALL, "");
 	char	*c = "coucou";
-	int		i = -8214;
+	int		i = 8214;
 
 
-	ft_printf("my ft_printf  \n");
-	ft_printf("MINUS:\n");
-	ft_printf("%-10lld\n", i);
-	ft_printf("%-+10lld\n", i);
-	ft_printf("%-#10lld\n", i);
-	ft_printf("%- 10lld\n", i);
-	ft_printf("%- +10lld\n", i);
-	ft_printf("%--10lld\n", i);
-	ft_printf("----------------------------------------\n");
+	// ft_printf("my ft_printf  \n");
+	// ft_printf("MINUS:\n");
+	// ft_printf("%-10d\n", i);
+	// ft_printf("%-+10d\n", i);
+	// ft_printf("%-#10d\n", i);
+	// ft_printf("%- 10d\n", i);
+	// ft_printf("%- +10d\n", i);
+	// ft_printf("%--10d\n", i);
+	// ft_printf("----------------------------------------\n");
 
 
 
-	ft_printf("my ft_printf  \n");
-	ft_printf("PLUS:\n");
-	ft_printf("%+10lld\n", i);
-	ft_printf("%+-10lld\n", i);
-	ft_printf("%+#10lld\n", i);
-	ft_printf("%+010lld\n", i);
-	ft_printf("%+ 10lld\n", i);
-	ft_printf("%++10lld\n", i);
-	ft_printf("%+-0 10lld\n", i);
-	ft_printf("----------------------------------------\n");
+	// ft_printf("my ft_printf  \n");
+	// ft_printf("PLUS:\n");
+	// ft_printf("%+10d\n", i);
+	// ft_printf("%+-10d\n", i);
+	// ft_printf("%+#10d\n", i);
+	// ft_printf("%+010d\n", i);
+	// ft_printf("%+ 10d\n", i);
+	// ft_printf("%++10d\n", i);
+	// ft_printf("%+-0 10d\n", i);
+	// ft_printf("----------------------------------------\n");
 
-	ft_printf("my ft_printf  \n");
-	ft_printf("ZERO:\n");
-	ft_printf("%0+10lld\n", i);
-	ft_printf("%0-10lld\n", i);
-	ft_printf("%0#10lld\n", i);
-	ft_printf("%0010lld\n", i);
-	ft_printf("%0 10lld\n", i);
-	ft_printf("%0 -10lld\n", i);
-	ft_printf("----------------------------------------\n");
+	// ft_printf("my ft_printf  \n");
+	// ft_printf("ZERO:\n");
+	// ft_printf("%0+10d\n", i);
+	// ft_printf("%0-10d\n", i);
+	// ft_printf("%0#10d\n", i);
+	// ft_printf("%0010d\n", i);
+	// ft_printf("%0 10d\n", i);
+	// ft_printf("%0 -10d\n", i);
+	// ft_printf("----------------------------------------\n");
 
-	ft_printf("my ft_printf  \n");
-	ft_printf("SPACE:\n");
-	ft_printf("% +10lld\n", i);
-	ft_printf("% -10lld\n", i);
-	ft_printf("% #10lld\n", i);
-	ft_printf("% 010lld\n", i);
-	ft_printf("%  10lld\n", i);
-	ft_printf("%  -10lld\n", i);
-	ft_printf("----------------------------------------\n");
-	ft_printf("----------------------------------------\n");
+	// ft_printf("my ft_printf  \n");
+	// ft_printf("SPACE:\n");
+	// ft_printf("% +10d\n", i);
+	// ft_printf("% -10d\n", i);
+	// ft_printf("% #10d\n", i);
+	// ft_printf("% 010d\n", i);
+	// ft_printf("%  10d\n", i);
+	// ft_printf("%  -10d\n", i);
+	// ft_printf("----------------------------------------\n");
+	// ft_printf("----------------------------------------\n");
 
 	printf("real printf :\n");
 	printf("MINUS:\n");
-	printf("%-10lld\n", i);
-	printf("%-+10lld\n", i);
-	printf("%-#10lld\n", i);
-	printf("%- 10lld\n", i);
-	printf("%- +10lld\n", i);
-	printf("%--10lld\n", i);
+	printf("%-10d\n", i);
+	printf("%-+10d\n", i);
+	printf("%-#10d\n", i);
+	printf("%- 10d\n", i);
+	printf("%- +10d\n", i);
+	printf("%--10d\n", i);
 	printf("----------------------------------------\n");
 
 	printf("real printf :\n");
 	printf("PLUS:\n");
-	printf("%+10lld\n", i);
-	printf("%+-10lld\n", i);
-	printf("%+#10lld\n", i);
-	printf("%+010lld\n", i);
-	printf("%+ 10lld\n", i);
-	printf("%++10lld\n", i);
-	printf("%+-0 10lld\n", i);
+	printf("%+10d\n", i);
+	printf("%+-10d\n", i);
+	printf("%+#10d\n", i);
+	printf("%+010d\n", i);
+	printf("%+ 10d\n", i);
+	printf("%++10d\n", i);
+	printf("%+-0 10d\n", i);
 	printf("----------------------------------------\n");
 
 
 	printf("real printf :\n");
 	printf("ZERO:\n");
-	printf("%0+10lld\n", i);
-	printf("%0-10lld\n", i);
-	printf("%0#10lld\n", i);
-	printf("%0010lld\n", i);
-	printf("%0 10lld\n", i);
-	printf("%0 -10lld\n", i);
+	printf("%0+10d\n", i);
+	printf("%0-10d\n", i);
+	printf("%0#10d\n", i);
+	printf("%0010d\n", i);
+	printf("%0 10d\n", i);
+	printf("%0 -10d\n", i);
 	printf("----------------------------------------\n");
 
 
 	printf("SPACE:\n");
-	printf("% +10lld\n", i);
-	printf("% -10lld\n", i);
-	printf("% #10lld\n", i);
-	printf("% 010lld\n", i);
-	printf("%  10lld\n", i);
-	printf("%  -10lld\n", i);
-	// printf("%10lld\n", i);
-	// printf("%-+10lld\n", i);
-	// printf("% 010lld\n", i);
-	// printf("%+010lld\n", i);
-	// printf("%--10lld\n", i);
-	// printf("%-010lld\n", i);
-	// printf("% +10lld\n", i);
+	printf("% +10d\n", i);
+	printf("% -10d\n", i);
+	printf("% #10d\n", i);
+	printf("% 010d\n", i);
+	printf("%  10d\n", i);
+	printf("%  -10d\n", i);
+	// printf("%10d\n", i);
+	// printf("%-+10d\n", i);
+	// printf("% 010d\n", i);
+	// printf("%+010d\n", i);
+	// printf("%--10d\n", i);
+	// printf("%-010d\n", i);
+	// printf("% +10d\n", i);
 
 
 
