ce7e91e609d12eee31dc03e2e865bbebb5688d18
Mohamed DCHANE <mdchane@e3r3p10.42.fr>
2019-01-08 15:55:05 +0100

    normed

--- printf.c
+++ printf.c
@@ -6,7 +6,7 @@
 /*   By: mdchane <mdchane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 12:27:25 by mdchane           #+#    #+#             */
-/*   Updated: 2019/01/08 11:18:59 by mdchane          ###   ########.fr       */
+/*   Updated: 2019/01/08 14:52:56 by mdchane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_printf(const char *format, ...)
 {
 	va_list	av;
 	size_t	i;
-	t_func  *tab;
+	t_func	*tab;
 	size_t	nb_print;
 
 	nb_print = 0;
@@ -28,9 +28,6 @@ int		ft_printf(const char *format, ...)
 		nb_print += ft_putstr_to_c(format + i, &i, '%');
 		if (format[i] == '%')
 			i += print_format(av, format + i + 1, tab, &nb_print) + 1;
-		// else
-		// 	nb_print += ft_putchar(format[i]);
-		// i++;
 	}
 	va_end(av);
 	ft_memdel((void **)&tab);
